1ee55b95db1e343835ff8cdf58341e1f1857c2a6
Almita05 <martinezlanderoalmalilia@gmail.com>
2023-11-11 16:28:38 -0600

    Añadí el evento onclick de las imagenes

---
+++ app/src/main/java/com/example/biomeztli/MainActivity2.java
@@ -0,0 +1,45 @@
+package com.example.biomeztli;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.firebase.ui.database.FirebaseRecyclerOptions;
+import com.google.firebase.database.FirebaseDatabase;
+
+public class MainActivity2 extends AppCompatActivity {
+
+    private RecyclerView recyclerView;
+    private MainAdapter mainAdapter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+
+        recyclerView = findViewById(R.id.rv);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+
+        FirebaseRecyclerOptions<MainModel> options =
+                new FirebaseRecyclerOptions.Builder<MainModel>()
+                        .setQuery(FirebaseDatabase.getInstance().getReference().child("plantas"), MainModel.class)
+                        .build();
+
+        mainAdapter = new MainAdapter(options);
+        recyclerView.setAdapter(mainAdapter);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        mainAdapter.startListening();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        mainAdapter.stopListening();
+    }
+}
---
+++ app/src/main/java/com/example/biomeztli/MainAdapter.java
@@ -0,0 +1,52 @@
+package com.example.biomeztli;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.bumptech.glide.Glide;
+import com.firebase.ui.database.FirebaseRecyclerAdapter;
+import com.firebase.ui.database.FirebaseRecyclerOptions;
+
+import de.hdodenhof.circleimageview.CircleImageView;
+
+public class MainAdapter extends FirebaseRecyclerAdapter<MainModel, MainAdapter.MyViewHolder> {
+
+    public MainAdapter(@NonNull FirebaseRecyclerOptions<MainModel> options) {
+        super(options);
+    }
+
+    @Override
+    protected void onBindViewHolder(@NonNull MyViewHolder holder, int position, @NonNull MainModel model) {
+        holder.nombre.setText(model.getNombre());
+
+        Glide.with(holder.img.getContext())
+                .load(model.getImagen())
+                .placeholder(com.firebase.ui.database.R.drawable.common_google_signin_btn_icon_dark)
+                .circleCrop()
+                .error(com.google.firebase.database.R.drawable.common_google_signin_btn_icon_dark_normal)
+                .into(holder.img);
+    }
+
+    @NonNull
+    @Override
+    public MyViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.main_item, parent, false);
+        return new MyViewHolder(view);
+    }
+
+    public static class MyViewHolder extends RecyclerView.ViewHolder {
+        CircleImageView img;
+        TextView nombre;
+
+        public MyViewHolder(@NonNull View itemView) {
+            super(itemView);
+            img = itemView.findViewById(R.id.img1);
+            nombre = itemView.findViewById(R.id.nameText); // Corregido el nombre de la variable
+        }
+    }
+}
